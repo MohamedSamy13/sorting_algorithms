ebe056c8d0ddea34c9086005ae496e2e48b04644
MohamedSamy13 <mohammed7samy777@gmail.com>
2024-04-12 19:31:26 -0700

    fix

--- 104-heap_sort.c
+++ 104-heap_sort.c
@@ -1,41 +1,60 @@
 #include "sort.h"
+
 /**
-  * heap_sort - heap sort algorithm
-  * @array: array to sort
-  * @size: size of array
-  */
+ * heap_sort - sorts an array following the Heap sort algorithm
+ * @array: array of ints to sort
+ * @size: size of the array to sort
+ */
 void heap_sort(int *array, size_t size)
 {
-	int i, tmp;
-
-	for (i = size / 2 - 1; i >= 0; i--)
-		heapify(array, i, size, size);
-	for (i = size -1; i >=0; i--)
-	{
-		tmp = array[0];
-		array[0] = array[i];
-		array[i] = tmp;
-		print_array(array, size);
-		heapify(array, i, 0, size);
-	}
-}
-void heapify(int *array, int idx, int idx2, size_t size)
-{
-	int max = idx2;
-	int left = 2 * idx2 + 1;
-	int right = 2 * idx2 + 2;
+	int i;
 	int tmp;
 
-	if (left < idx && array[left] > array[max])
-		max = left;
-	if (right < idx && array[right] > array[max])
-		max = right;
-	if (max != idx2)
+	if (size < 2)
+		return;
+
+	for (i = size / 2 - 1; i >= 0; i--)
+		heapify(array, size, (size_t)i, size);
+
+	for (i = size - 1; i >= 0; i--)
 	{
-		tmp = array[idx2];
-		array[idx2] = array[max];
+		tmp = array[i];
+		array[i] = array[0];
+		array[0] = tmp;
+		if (i != 0)
+			print_array(array, size);
+		heapify(array, (size_t)i, 0, size);
+	}
+}
+
+/**
+ * heapify - turns an array in a heap tree
+ * @array: array to turn into heap
+ * @s: size of the subtree
+ * @root: index of the subtree in the heap
+ * @size: size of the whole array
+ */
+void heapify(int *array, size_t s, size_t root, size_t size)
+{
+	size_t max, left, right;
+	int tmp;
+
+	max = root;
+	left = (root * 2) + 1;
+	right = (root * 2) + 2;
+
+	if (left < s && array[left] > array[max])
+		max = left;
+
+	if (right < s && array[right] > array[max])
+		max = right;
+
+	if (max != root)
+	{
+		tmp = array[root];
+		array[root] = array[max];
 		array[max] = tmp;
 		print_array(array, size);
-		heapify(array, idx, max, size);
+		heapify(array, s, max, size);
 	}
 }
